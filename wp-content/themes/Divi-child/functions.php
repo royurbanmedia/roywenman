eac395955a5df1fd108e34c7e4d011448f035779
Roy <doihaveto@gmail.com>
2018-07-26 20:38:09 +0100

    Add tools icons

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -8,6 +8,10 @@ function my_theme_enqueue_styles() {
 
     wp_enqueue_script('bxslider-js', '//cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js', array('jquery'));
 
+    $templateURL = get_stylesheet_directory_uri();
+
+    wp_localize_script( 'bxslider-js', 'php', $templateURL );
+
     wp_enqueue_script('bxslider-config', get_stylesheet_directory_uri() . '/js/bxslider-config.js', array('jquery', 'bxslider-js'));
 
     wp_enqueue_script('particles-js', '//cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.2/particles.min.js');
--- wp-content/themes/Divi-child/js/bxslider-config.js
+++ wp-content/themes/Divi-child/js/bxslider-config.js
@@ -1,16 +1,30 @@
 jQuery(document).ready(function($) {
 
   function changeSlideTools(slide) {
-    var projectTools = {
-      0: {
-        tools: ['wordpress', 'lifterlms', 'paypal']
-      },
-      1: {
-        tools: ['wordpress', 'woocommerce', 'stripe']
-      }
-    };
+    var tools;
+    var templateDirectory = php.templateURL;
 
-    console.log('this project tools are ' + projectTools.slide.tools);
+    switch(slide) {
+      case 0:
+        tools = ['wordpress', 'lifterlms', 'activecampaign'];
+      break;
+
+      case 1:
+        tools = ['wordpress', 'woocommerce', 'paypal'];
+      break;
+    }
+
+    $( ".tool1, .tool2, .tool3" ).fadeOut( "slow", function() {
+      $('.tool1').attr("src", templateDirectory + '/' + tools[0] + '.png');
+      $('.tool2').attr("src", templateDirectory + '/' + tools[1] + '.png');
+      $('.tool3').attr("src", templateDirectory + '/' + tools[2] + '.png');
+
+      $( ".tool1, .tool2, .tool3" ).fadeIn("slow", function(){
+        console.log('change complete ' + templateDirectory + '/' + tools[0] + '.png')
+      });
+    });
+
+
   }
 
   $('.slider').bxSlider({
